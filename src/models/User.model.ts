af417cc337a73c03ece521ad4c5801b9fab4da2c
Qavi-Nizamani <qaviniz11@gmail.com>
2024-10-23 17:34:58 +0500

    Updated User model file

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,17 +1,25 @@
 // Create a User Model to store user data in the MongoDb database
 
 import mongoose, { model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 export interface UserDocument {
   email: string;
-  passwordHash: string;
+  password: string;
 }
 
 const UserSchema = new Schema<UserDocument>({
   email: { type: String, required: true },
-  passwordHash: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.models?.User || model<UserDocument>("User", UserSchema);
 
 export default User;
+
+
+export const saltAndHashPassword = (password: string) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+};
